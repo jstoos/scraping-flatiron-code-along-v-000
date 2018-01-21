e2c3bf179ecf6d3377cafd694cb9eca2a9b17d6a
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-21 19:38:12 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,17 @@
 class Course
   attr_accessor :title, :schedule, :description
 
+ @@all = []
+ 
+  def initialize(course)
+    self.all << course
+  end
+  
   def self.all
+    @@all
   end
 
   def self.reset_all
   end
-  
+
 end
